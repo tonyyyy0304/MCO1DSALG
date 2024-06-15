041846350269813396f8f871947d68bee34684c4
tonytonytony <anthonybaybayon83@gmail.com>
2024-06-15 19:30:56 +0800

    updated queues

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -34,16 +34,33 @@ void push(token10 stack[], char* src, int* top)
     (*top)++;
 }
 
-
-
-void tokenExtractor(char* stringInput, token10 stack[])
+int enqueue(token10 queue[], char* src, int* tail, int* head)
 {
-    int i, topIndex = 0;
+
+    if(*tail == 254)
+        return 0;
+    else
+    {
+        if (*head == -1)
+            *head = 0;
+        *tail += 1;
+        strcpy(queue[*tail], src);
+        return 1;
+    } 
+}
+
+
+
+void tokenExtractor(char* stringInput, token10 queue[], int* headIndex, int* tailIndex)
+{
+    int i;
+
+    *headIndex = -1, *tailIndex = -1;
+
     char temp[100] = "\0";
 
     for (i=0; i<strlen(stringInput); i++)
     {
-      
 
         if(isDigit(stringInput[i]))
         {
@@ -51,7 +68,7 @@ void tokenExtractor(char* stringInput, token10 stack[])
 
             if(!isDigit(stringInput[i+1]))
             {
-                push(stack, temp, &topIndex);
+                enqueue(queue, temp, tailIndex, headIndex);
                 strcpy(temp, "\0");
             }
         }
@@ -61,15 +78,16 @@ void tokenExtractor(char* stringInput, token10 stack[])
 
             if(!isOperator(stringInput[i+1]))
             {
-                push(stack, temp, &topIndex);
+                enqueue(queue, temp, tailIndex, headIndex);
                 strcpy(temp, "\0");
             }
         }
         else if(isParenthesis(stringInput[i]))
         {
             strncat(temp, &stringInput[i], 1);
-            push(stack, temp, &topIndex);
+            enqueue(queue, temp, tailIndex, headIndex);
             strcpy(temp, "\0");
         }
     }
 }
+
