e3273ab3d1ecc1674189163ba79a43dbd7279d94
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-06-20 18:33:22 +0800

    Update evaluatePostfix.c

--- evaluatePostfix.c
+++ evaluatePostfix.c
@@ -1,14 +1,13 @@
 
 int postfixAnswer(int operand1, int operand2, char* operator) {
     
-    
     if (strcmp(operator, "^") == 0){
         return pow(operand1, operand2);
     }else if (strcmp(operator, "*") == 0){
         return operand1 * operand2;   
     }else if (strcmp(operator, "/") == 0){
         if (operand2 == 0){
-            printf("Division by zero error!");
+            return -1;
         }
         else return operand1 / operand2;
     }else if (strcmp(operator, "%") == 0){
